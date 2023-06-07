d020bbf7715e1ba9d046b425315c09ecfc881151
Cristiano Cândido da Silva <cristiano.objetiva.lg@gmail.com>
2023-06-07 11:22:51 -0300

    Projeto Web

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function colorChange(cor){
+    let imgCirculo = document.querySelector(".img-circulo")
+    imgCirculo.style.background = cor;
+}
+
+function imgChange(imagens){
+    let imgIphone = document.querySelector(".iphone")
+    imgIphone.src = imagens
+
+}
